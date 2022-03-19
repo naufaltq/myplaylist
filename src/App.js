351e978ae5ea0f1e5bdfcd4f5c72c49c4f644888
Naufal Taufiqurrahman <naufaltq16@gmail.com>
2022-03-19 20:07:21 +0700

    add .env and modified app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const spotify_secret_key = process.env.REACT_APP_SPOTIFY_KEY;
   return (
     <div className="App">
       <header className="App-header">
@@ -9,12 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
