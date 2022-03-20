1fb7f294638e41cdf8d8978552cd6fb5c27c3196
Naufal Taufiqurrahman <naufaltq16@gmail.com>
2022-03-20 10:43:19 +0700

    modified src/index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-const spotify_secret_key = process.env.REACT_APP_SPOTIFY_KEY;
 
 ReactDOM.render(
   <React.StrictMode>
